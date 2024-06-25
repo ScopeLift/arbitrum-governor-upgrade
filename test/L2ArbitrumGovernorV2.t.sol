f19a3e7f85c07ec58ff9a5bb1ce46b3f4a4be211
Gary Ghayrat <61768337+garyghayrat@users.noreply.github.com>
2024-06-25 10:24:53 -0400

    Add `EXCLUDE_ADDRESS` feature (#8)
    * Add `EXCLUDE_ADDRESS` feat
    
    * Refactor tests
    
    * Use `assertGt` instead of `assertTrue`
    
    * Rename var

--- test/L2ArbitrumGovernorV2.t.sol
+++ test/L2ArbitrumGovernorV2.t.sol
@@ -13,6 +13,7 @@ import {TimelockControllerUpgradeable} from "openzeppelin-upgradeable/governance
 import {IVotes} from "openzeppelin/governance/utils/IVotes.sol";
 import {TransparentUpgradeableProxy} from "openzeppelin/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ERC20Mock} from "openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
+import {ERC20VotesUpgradeable} from "openzeppelin-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 
 // ----------------------------------------------------------------------------------------------------------------- //
 // Test Suite Base - Shared values, setup, helpers, and virtual methods needed by concrete test contracts
@@ -25,12 +26,13 @@ abstract contract L2ArbitrumGovernorV2Test is Test, SharedGovernorConstants {
   address constant PROXY_ADMIN_CONTRACT = 0x740f24A3cbF1fbA1226C6018511F96d1055ce961; // Proxy Admin Contract Address
   L2ArbitrumGovernorV2 governor;
   BaseGovernorDeployer proxyDeployer;
+  ERC20VotesUpgradeable arbitrumToken;
   ERC20Mock mockToken;
 
   // Each concrete test suite returns the appropriate concrete deploy script which will be exercised in setup
   function _createGovernorDeployer() internal virtual returns (BaseGovernorDeployer);
 
-  function setUp() public {
+  function setUp() public virtual {
     vm.createSelectFork(
       vm.envOr("ARBITRUM_ONE_RPC_URL", string("Please set ARBITRUM_ONE_RPC_URL in your .env file")), FORK_BLOCK
     );
@@ -38,6 +40,7 @@ abstract contract L2ArbitrumGovernorV2Test is Test, SharedGovernorConstants {
     DeployImplementation _implementationDeployer = new DeployImplementation();
     _implementationDeployer.setUp();
     address _implementation = address(_implementationDeployer.run());
+    arbitrumToken = ERC20VotesUpgradeable(ARB_TOKEN_ADDRESS);
     mockToken = new ERC20Mock();
 
     proxyDeployer = _createGovernorDeployer();
@@ -56,7 +59,7 @@ abstract contract Initialize is L2ArbitrumGovernorV2Test {
     assertEq(governor.votingDelay(), INITIAL_VOTING_DELAY);
     assertEq(governor.votingPeriod(), INITIAL_VOTING_PERIOD);
     assertEq(governor.proposalThreshold(), INITIAL_PROPOSAL_THRESHOLD);
-    assertEq(address(governor.token()), address(ARB_TOKEN_ADDRESS));
+    assertEq(address(arbitrumToken), address(ARB_TOKEN_ADDRESS));
     assertEq(address(governor.timelock()), proxyDeployer.TIMELOCK_ADDRESS());
     assertEq(governor.lateQuorumVoteExtension(), INITIAL_VOTE_EXTENSION);
     assertEq(governor.owner(), GOVERNOR_OWNER);
@@ -124,6 +127,75 @@ abstract contract Relay is L2ArbitrumGovernorV2Test {
   }
 }
 
+abstract contract Quorum is L2ArbitrumGovernorV2Test {
+  function setUp() public override {
+    super.setUp();
+    _setQuorumNumerator(3000); // 30% quorum
+    vm.roll(vm.getBlockNumber() + 1);
+  }
+
+  function _setQuorumNumerator(uint256 _numerator) internal {
+    vm.prank(address(governor));
+    governor.updateQuorumNumerator(_numerator);
+  }
+
+  function _getMajorTokenHolder(uint256 _actorSeed) internal pure returns (address) {
+    address[] memory _majorTokenHolders = new address[](4);
+    _majorTokenHolders[0] = 0x62383739D68Dd0F844103Db8dFb05a7EdED5BBE6;
+    _majorTokenHolders[1] = 0xF977814e90dA44bFA03b6295A0616a897441aceC;
+    _majorTokenHolders[2] = 0xf7F468B184A48f6ca37EeFFE12733Ee1c16B6E26;
+    _majorTokenHolders[3] = 0x1190CEA3e2c8727218768bFb990C3228aA06dfA9;
+    return _majorTokenHolders[_actorSeed % _majorTokenHolders.length];
+  }
+
+  function testFuzz_ReturnsCorrectQuorum(uint256 _numerator, uint256 _pastBlockNumber) public {
+    _numerator = bound(_numerator, 1, governor.quorumDenominator());
+    _setQuorumNumerator(_numerator);
+    vm.roll(vm.getBlockNumber() + 1);
+    _pastBlockNumber = bound(_pastBlockNumber, 1, vm.getBlockNumber() - 1);
+
+    uint256 tokenPastTotalSupply = arbitrumToken.getPastTotalSupply(_pastBlockNumber);
+    uint256 excludeAddressVotes = arbitrumToken.getPastVotes(governor.EXCLUDE_ADDRESS(), _pastBlockNumber);
+    uint256 expectedQuorum = (tokenPastTotalSupply - excludeAddressVotes) * governor.quorumNumerator(_pastBlockNumber)
+      / governor.quorumDenominator();
+    vm.assertEq(governor.quorum(_pastBlockNumber), expectedQuorum);
+  }
+
+  function testFuzz_ReturnsCorrectQuorumAfterDelegatingToExcludeAddress(uint256 _numerator, uint256 _actorSeed) public {
+    // Set a random numerator
+    _numerator = bound(_numerator, 1, governor.quorumDenominator());
+    _setQuorumNumerator(_numerator);
+    vm.roll(vm.getBlockNumber() + 1);
+
+    // Keep track of the previous quorum and exclude address votes
+    uint256 previousQuorum = governor.quorum(vm.getBlockNumber() - 1);
+    uint256 previousExcludeAddressVotes =
+      arbitrumToken.getPastVotes(governor.EXCLUDE_ADDRESS(), vm.getBlockNumber() - 1);
+
+    // Delegate a major token holder's balance to the exclude address
+    address _actor = _getMajorTokenHolder(_actorSeed);
+    vm.startPrank(_actor);
+    arbitrumToken.delegate(governor.EXCLUDE_ADDRESS());
+    vm.stopPrank();
+    vm.roll(vm.getBlockNumber() + 1);
+
+    uint256 previousBlock = vm.getBlockNumber() - 1;
+    uint256 tokenPastTotalSupply = arbitrumToken.getPastTotalSupply(previousBlock);
+    uint256 excludeAddressVotes = arbitrumToken.getPastVotes(governor.EXCLUDE_ADDRESS(), previousBlock);
+    uint256 expectedQuorum = (tokenPastTotalSupply - excludeAddressVotes) * governor.quorumNumerator(previousBlock)
+      / governor.quorumDenominator();
+
+    vm.assertEq(governor.quorum(previousBlock), expectedQuorum);
+    // Exclude address votes should increase by the actor's balance
+    vm.assertEq(
+      arbitrumToken.getPastVotes(governor.EXCLUDE_ADDRESS(), previousBlock),
+      previousExcludeAddressVotes + arbitrumToken.balanceOf(_actor)
+    );
+    // Quorum should decrease
+    vm.assertGt(previousQuorum, expectedQuorum);
+  }
+}
+
 // ----------------------------------------------------------------------------------------------------------------- //
 // Concrete Test Contracts - Inherit from each abstract test and implement concrete methods for Core & Treasury case
 // ----------------------------------------------------------------------------------------------------------------- //
@@ -140,6 +212,12 @@ contract CoreGovernorRelay is Relay {
   }
 }
 
+contract CoreGovernorQuorum is Quorum {
+  function _createGovernorDeployer() internal override returns (BaseGovernorDeployer) {
+    return new DeployCoreGovernor();
+  }
+}
+
 contract TreasuryGovernorInitialize is Initialize {
   function _createGovernorDeployer() internal override returns (BaseGovernorDeployer) {
     return new DeployTreasuryGovernor();
@@ -147,6 +225,12 @@ contract TreasuryGovernorInitialize is Initialize {
 }
 
 contract TreasuryGovernorRelay is Relay {
+  function _createGovernorDeployer() internal override returns (BaseGovernorDeployer) {
+    return new DeployTreasuryGovernor();
+  }
+}
+
+contract TreasuryGovernorQuorum is Quorum {
   function _createGovernorDeployer() internal override returns (BaseGovernorDeployer) {
     return new DeployTreasuryGovernor();
   }
