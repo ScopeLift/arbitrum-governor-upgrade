65ec04ad031f1533f34d346820f15b3ed027ed81
Gary Ghayrat <61768337+garyghayrat@users.noreply.github.com>
2024-07-26 12:05:17 -0400

    Pin flexible-voting contract version to 0.8.26 (#26)

--- src/lib/governance/extensions/GovernorCountingFractionalUpgradeable.sol
+++ src/lib/governance/extensions/GovernorCountingFractionalUpgradeable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity 0.8.26;
 
 import {GovernorUpgradeable} from "openzeppelin-upgradeable/governance/GovernorUpgradeable.sol";
 import {GovernorCountingSimpleUpgradeable} from
